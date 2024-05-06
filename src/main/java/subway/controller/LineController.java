696159d8e808e3a1bc2884181d8fff7d097f17f6
aristo <dkfud2121@naver.com>
2024-05-06 22:43:42 +0900

    doc: 서비스 초기 흐름 설정

---
+++ src/main/java/subway/controller/LineController.java
@@ -0,0 +1,6 @@
+package subway.controller;
+
+public class LineController {
+    public LineController(){}
+
+}
--- src/main/java/subway/controller/MainController.java
+++ src/main/java/subway/controller/MainController.java
@@ -1,5 +1,31 @@
 package subway.controller;
 
-public class MainController {
+import subway.view.Ask;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MainController {
+    Ask ask = new Ask();
+    public MainController(){
+        ask.Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String command = br.readLine();
+            if(command.equals("Q")) {
+                System.out.println(" 안녕히 가세요. ");
+                return;
+            }
+            int validCommand = Integer.parseInt(command);
+            CoreController(validCommand);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void CoreController(int command){
+        if(command == 1) new StationController();
+        if(command == 2) new LineController();
+        if(command == 3) new SectionController();
+    }
 }
