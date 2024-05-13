938675866a13efbde185dda57d3e8f9a38947024
aristo <dkfud2121@naver.com>
2024-05-13 21:27:22 +0900

    refactor: error handler based on Errors ENUM file

--- src/main/java/subway/config/handler/SubwayException.java
+++ src/main/java/subway/config/handler/SubwayException.java
@@ -1,19 +1,21 @@
 package subway.config.handler;
 
+import subway.config.constants.views.Errors;
 import subway.config.constants.views.Prefixes;
 
 public class SubwayException extends IllegalArgumentException {
+    private static String ERROR = Prefixes.ERROR.getPrefix();
     public void notValidCommand() {
-        throw new IllegalArgumentException(Prefixes.ERROR.getPrefix()+"잘못된 명령입니다.");
+        throw new IllegalArgumentException(ERROR+ Errors.VALID.getError());
     }
 
     public void areadyCreated(){
-        throw new IllegalArgumentException(Prefixes.ERROR.getPrefix()+"이미 존재하는 노선입니다.");
+        throw new IllegalArgumentException(ERROR+Errors.ADD_LINE.getError());
     }
 
 
     public void unexpected(){
-        throw new IllegalArgumentException(Prefixes.ERROR.getPrefix()+"예상치 못한 에러가 발생했습니다.");
+        throw new IllegalArgumentException(ERROR+Errors.UNEXPECTED.getError());
     }
     public boolean isBack(String command){
         if(command.equals('B')){
@@ -23,15 +25,15 @@ public class SubwayException extends IllegalArgumentException {
     }
 
     public void checkCommand() {
-        throw new IllegalArgumentException(Prefixes.ERROR.getPrefix()+ "입력값을 확인해주세요.");
+        throw new IllegalArgumentException(ERROR+ Errors.VALID.getError());
     }
 
     public void noStation() {
-        throw new IllegalArgumentException(Prefixes.ERROR.getPrefix()+"존재하지 않는 역입니다.");
+        throw new IllegalArgumentException(ERROR+Errors.DEL_STATION.getError());
     }
 
     public void noLine() {
-        throw new IllegalArgumentException(Prefixes.ERROR.getPrefix()+"존재하지 않는 노선입니다.");
+        throw new IllegalArgumentException(ERROR+Errors.DEL_LINE.getError());
     }
 
 
