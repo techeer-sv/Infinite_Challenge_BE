68c6d35d5fadbdc6c31da90eb35eaec8e40988cc
aristo <dkfud2121@naver.com>
2024-05-17 17:34:02 +0900

    refactor: SectionController 중심 코드 정리

--- src/main/java/subway/service/SectionManager.java
+++ src/main/java/subway/service/SectionManager.java
@@ -18,8 +18,9 @@ public class SectionManager implements Managerable {
         return isStationEmpty(station);
     }
     public boolean isEmpty(final String type, final String name){
+        if(subwayException.isBack(name) == true) return false; // TODO: 종료시키는 에러? 커스텀?
         if(type.equals(Targets.STATION.getTarget())) return isStationEmpty(name);
-        if(type.equals(Targets.LINE.getTarget())) return isStationEmpty(name);
+        if(type.equals(Targets.LINE.getTarget())) return isLineEmpty(name);
         return true;
     }
 
@@ -37,15 +38,6 @@ public class SectionManager implements Managerable {
         Station station = stationRepo.getStationByName(sName);
         if(station == null) subwayException.noStation();
         lineRepo.addLine(line, station, index);
-    }
-
-    public boolean getStation(String name){
-        try{
-            stationRepo.getStationByName(name);
-            return true;
-        }catch (Exception e) {
-        }
-        return false;
     }
 
     public Line getLine(String name){
