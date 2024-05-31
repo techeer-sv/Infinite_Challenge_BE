ee291829f10d8a89539ed09ead2cd3ed0848a0a0
heondong9265 <heondong0723@gmail.com>
2024-05-31 21:50:43 +0900

    지하철 등록 #10 refactor : ENUM 리팩토링

--- src/main/java/subway/domain/enums/LineEnum.java
+++ src/main/java/subway/domain/enums/LineEnum.java
@@ -1,15 +1,18 @@
 package subway.domain.enums;
 
+import java.util.Arrays;
+
 public enum LineEnum {
 
-    LINE_2("2호선", new String[]{"교대역", "강남역", "역삼역"}),
-    LINE_3("3호선", new String[]{"교대역", "남부터미널역", "양재역", "매봉역"}),
-    SIN_BUNDANG("신분당선", new String[]{"강남역", "양재역", "양재시민의숲역"});
+    LINE_2("2호선", new StationEnum[]{StationEnum.GYO_DAE, StationEnum.GANG_NAM, StationEnum.YEOK_SAM}),
+    LINE_3("3호선", new StationEnum[]{StationEnum.GYO_DAE, StationEnum.NAM_BU_TERMINAL, StationEnum.YANG_JAE, StationEnum.MAE_BONG}),
+    LINE_SIN_BUNDANG("신분당선", new StationEnum[]{StationEnum.GANG_NAM, StationEnum.YANG_JAE, StationEnum.YANG_JAE_CITIZENS_FOREST});
+
 
     private final String name;
-    private final String[] stations;
+    private final StationEnum[] stations;
 
-    LineEnum(String name, String[] stations) {
+    LineEnum(String name, StationEnum[] stations) {
         this.name = name;
         this.stations = stations;
     }
@@ -19,6 +22,8 @@ public enum LineEnum {
     }
 
     public String[] getStations() {
-        return stations;
+        return Arrays.stream(stations)
+                .map(StationEnum::getName)
+                .toArray(String[]::new);
     }
 }
